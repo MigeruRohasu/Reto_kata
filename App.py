8bd29f4097d7bd4fc5cf81937557cbb796ba0fbc
MigeruRohasu <94565517+MigeruRohasu@users.noreply.github.com>
2023-11-13 08:33:51 -0500

    primer ejercicio

---
+++ App.py
@@ -0,0 +1,33 @@
+s=8
+arreglo=[]
+
+def crearlista(n):
+    for i in range(0,n):
+        arreglo.append(i)
+
+def borrar_numeros(arr,s):
+    global arreglo
+    new_arr=[]
+    
+    for ii in range(0,len(arr)):
+        for i in range(0,len(arr)):
+            if arr[i] > 9:
+                num = arr.pop(i)              
+                arr.insert(i,int(num/10))
+                arr.insert(i+1,num%10)
+                break
+         
+    for ii in range(0,len(arr)):
+        for i in range(0,len(arr)-1):             
+            if arr[i] == s:
+                arr.pop(i)
+                break
+
+    for i in range(len(arr)-1,0,-1):
+        new_arr.append(arr[i])
+    arreglo=new_arr
+
+
+crearlista(s)
+borrar_numeros(arreglo,s)
+print(arreglo)
