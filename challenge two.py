e0b5568a842765f885e14fca3e1284b3485621b8
MigeruRohasu <94565517+MigeruRohasu@users.noreply.github.com>
2023-11-13 12:35:06 -0500

    segundo v2

--- challenge two.py
+++ challenge two.py
@@ -1,33 +1,46 @@
 s=8
-arreglo=[6,7,8,9,10,1,2,3,4,5]
-
-def elevar(arr,n):
+arreglo=[6,7,8,9,10,2,8,9,10,1,2]
+#arreglo=[1,2,3,4,5,6,7,8,9,10,11,12,3,4,5,6,7,8,9,10,11,1,8,9,10,11,12,3,4,5]
+def elevar(arr,nn):
     global arreglo
 
     for i in range(0,len(arr)):
         arr.insert(i,pow(arr.pop(i),2))
     
-    num=n+(n*10)    
-    for i in range(0,len(arr)-1):
-        if arr[i] >= num:
-            del arr[i]
-    
+   
+
+    print(arr)
+
     new_arr=arr 
-    num=new_arr[0]
-    
+    num1=0
     for ii in range (0,len(arr)):
-        num=new_arr[ii]
-        for i in range(ii,len(new_arr)):     
-            #print("new_arr[ii]",new_arr[ii],"new_arr[i]",new_arr[i], "num", num)   
-            if new_arr[i]<new_arr[ii] and num > new_arr[i]:
-                num=i
-            
+
+        num1=ii
+        for i in range(ii,len(new_arr)):        
+            if new_arr[i]<new_arr[ii] and new_arr[num1] > new_arr[i]:
+                num1=i
+            elif new_arr[i]<new_arr[ii] and new_arr[num1] > new_arr[i]:
+                num1=i
         try:
-            new_arr.insert(ii,new_arr[num])
-            del new_arr[num+1]
+            #new_arr.insert(ii,new_arr[num])
+            #del new_arr[num+1]
+            new_arr.insert(ii,new_arr.pop(num1))
         except:
             n=0
-        
+
+
+    print(new_arr)
+    num=nn+(nn*10)    
+    num1=len(new_arr)
+    for i in range(0,num1):   
+        for ii in range(0,num1): 
+            try:      
+                if new_arr[ii] >= num:
+
+                    del new_arr[ii]
+            except:
+                n=0
+      
     print(new_arr)
 
 
